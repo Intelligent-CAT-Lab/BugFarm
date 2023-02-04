d42d23792813f36f9c80a1ab81211003590c2977
alibrahimzada <alirezai@illinois.edu>
2023-02-03 22:59:22 -0600

    updated method extraction scripts

--- method_extractor.py
+++ method_extractor.py
@@ -1,183 +1,114 @@
 import javalang
 import json
 import os
-from multiprocessing import Pool
+import multiprocessing
 import argparse
 import sys
+import subprocess
+import glob
+import time
 from typing import Set, Tuple
 from utils import tokenize
-import ast
+import logging
+
+
+class Counter(object):
+    def __init__(self):
+        self.val = multiprocessing.Value('i', 0)
 
 
 def parse_java_func_intervals(content: str) -> Set[Tuple[int, int]]:
     func_intervals = set()
-    for _, node in javalang.parse.parse(content):
-        if isinstance(
-            node,
-            (javalang.tree.MethodDeclaration, javalang.tree.ConstructorDeclaration),
-        ):
-            func_intervals.add(
-                (
-                    node.start_position.line,
-                    node.end_position.line + 1,
+    try:
+        for _, node in javalang.parse.parse(content):
+            if isinstance(
+                node,
+                (javalang.tree.MethodDeclaration, javalang.tree.ConstructorDeclaration),
+            ):
+                func_intervals.add(
+                    (
+                        node.start_position.line,
+                        node.end_position.line + 1,
+                    )
                 )
-            )
-    return func_intervals
+        return func_intervals
+    except javalang.parser.JavaSyntaxError:
+        return func_intervals
 
 
-def get_class_paths(project, bug_id, modified_classes):
-    full_path = []
-    for class_name in modified_classes:
-        java_file_name = class_name.split(".")[-1] + ".java"
-        class_fixed_pwd = "./projects/" + project + "/" + bug_id
+def process_file(target_file):
 
-        os.system("find " + class_fixed_pwd + " -name " + "'" + java_file_name + "'" + " > " + f"{project}{bug_id}{class_name}.txt")
+    with open(target_file, mode='r', encoding="ISO-8859-1", errors='ignore') as r:
+        codelines = r.readlines()
+        code_text = ''.join(codelines)
 
-        with open(f"{project}{bug_id}{class_name}.txt", "r", encoding="ISO-8859-1", errors='ignore') as f:
-            found_paths = f.readlines()
-            if len(found_paths) == 0:
-                os.system(f"rm {project}{bug_id}{class_name}.txt")
-                continue
+    intervals = parse_java_func_intervals(code_text)
+    unique_name = target_file.replace('/', '.')
+    unique_name = unique_name.replace('$', '')
 
-            substring = class_name.replace('.', '/')
-            class_fixed_location = ''
-            for path in found_paths:
-                if substring in path:
-                    class_fixed_location = path
-                    break
+    for start, end in intervals:
+        with counter.val.get_lock():
+            index = counter.val.value
+            counter.val.value += 1
 
-            class_fixed_location = class_fixed_location.strip()
-            class_fixed_location = class_fixed_location.replace("$", "\$")
-            full_path.append(class_fixed_location)
+        method_text =  ''.join(codelines[start-1:end])
 
-        os.system(f"rm {project}{bug_id}{class_name}.txt")
-    
-    return full_path
+        if method_text.strip() == "": continue
 
+        with open(f'{unique_name}.{index}.java', mode='w', encoding="ISO-8859-1", errors='ignore') as fw:
+            fw.write(method_text)
 
-def get_bug_ids(project_name):
-    command = 'defects4j query -p ' + project_name + f' -q bug.id > {project_name}_bug_ids.txt'
-    os.system(command)
+        os.system(f'tokenizer {unique_name}.{index}.java > {unique_name}.{index}.txt')
+        tokens = tokenize(f'{unique_name}.{index}.txt')
 
-    bug_ids = []
-    with open(f'{project_name}_bug_ids.txt') as fr:
-        for line in fr.readlines():
-            bug_ids.append(line.strip())
+        instance = {}
+        instance['index'] = str(index)
+        instance['project'] = args.project_name
+        instance['file_path'] = target_file
+        instance['start_line'] = str(start)
+        instance['end_line'] = str(end)
+        instance['method'] = method_text
+        instance['tokens'] = tokens
 
-    os.system(f'rm {project_name}_bug_ids.txt')
-    return bug_ids
-
-
-def get_file_paths(project_name, bug_id):
-    modified_classes = []
-    with open(f'{project_name}_{bug_id}_relevant_classes.txt') as f:
-        modified_classes = f.readlines()
-        modified_classes = list(map(lambda x: x.strip(), modified_classes))
+        json_file.write(json.dumps(instance) + '\n')
+        json_file.flush()
         
-    os.system(f'rm {project_name}_{bug_id}_relevant_classes.txt')
-
-    return get_class_paths(project_name, bug_id, modified_classes)
-
-
-def process_bug(bug_id):
-    os.system(f'mkdir -p projects/{project}/{bug_id}')
-    os.system(f'defects4j checkout -p {project} -v {bug_id}f -w projects/{project}/{bug_id}')
-    os.system(f'defects4j export -p classes.relevant -w projects/{project}/{bug_id} > {project}_{bug_id}_relevant_classes.txt')
-    os.system(f'chmod -R 777 projects/{project}/{bug_id}')
-
-    file_paths = get_file_paths(project, bug_id)
-
-    index = 0
-    for target_file in file_paths:
-
-        if not os.path.exists(target_file): continue
-
-        with open(target_file, mode='r', encoding="ISO-8859-1", errors='ignore') as r:
-            codelines = r.readlines()
-            code_text = ''.join(codelines)
-
-        intervals = parse_java_func_intervals(code_text)
-
-        for start, end in intervals:
-            method_text =  ''.join(codelines[start-1:end])
-
-            if method_text.strip() == "": continue
-
-            with open(f'{project}.{bug_id}.{index}.java', mode='w', encoding="ISO-8859-1", errors='ignore') as fw:
-                fw.write(method_text)
-
-            os.system(f'tokenizer {project}.{bug_id}.{index}.java > {project}.{bug_id}.{index}.txt')
-            tokens = tokenize(f'{project}.{bug_id}.{index}.txt')
-
-            dataset = {}
-            dataset['index'] = str(index)
-            dataset['project'] = project
-            dataset['bug_id'] = bug_id
-            dataset['file_path'] = target_file
-            dataset['start_line'] = str(start)
-            dataset['end_line'] = str(end)
-            dataset['method'] = method_text
-            dataset['tokens'] = tokens
-
-            json_file.write(json.dumps(dataset) + '\n')
-            json_file.flush()
-            
-            os.remove(f'{project}.{bug_id}.{index}.txt')
-            os.remove(f'{project}.{bug_id}.{index}.java')
-
-            index += 1
-
-    os.system(f'rm -r projects/{project}/{bug_id}')
+        os.remove(f'{unique_name}.{index}.txt')
+        os.remove(f'{unique_name}.{index}.java')
 
 
 def main(args):
-    global project
+
+    start = time.time()
+
     global json_file
-    project = args.project_name
+    global counter
 
-    os.system(f'mkdir -p data/{project}')
-    json_file = open(f"data/{project}/methods.jsonl", "wt")
+    os.makedirs(f'logs', exist_ok=True)
+    logging.basicConfig(filename=f"logs/{args.log_file}", level=logging.INFO, format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(f'extracting methods from {args.project_name}')
 
-    # export project bug ids
-    bug_ids = get_bug_ids(project)
+    counter = Counter()
 
-    # multiprocessing for faster processing
-    pool = Pool(os.cpu_count())
-    for i, _ in enumerate(pool.imap_unordered(process_bug, bug_ids), 1):
-        sys.stderr.write('\rpercentage of files completed: {0:%}'.format(i/len(bug_ids)))
-    
-    # handle duplicates
-    lines = []
-    with open(f'data/{project}/methods.jsonl') as fr:
-        lines = fr.readlines()
+    files = subprocess.Popen(["ls"] + glob.glob(f'projects/{args.project_name}/**/src/main/java/**/*.java', recursive=True), stdout=subprocess.PIPE)
+    source_paths = [x.decode('ascii').strip() for x in files.stdout.readlines()]
 
-    json_file = open(f"data/{project}/methods.jsonl", "wt")
-    unique_methods = []
-    unique_method_tokens = []
+    os.makedirs(f'data/{args.project_name}', exist_ok=True)
 
-    for l in lines:
-        dct = ast.literal_eval(l)
+    json_file = open(f"data/{args.project_name}/unique_methods.jsonl", "wt")
+    pool = multiprocessing.Pool(os.cpu_count())
 
-        if dct['method'].strip() not in unique_methods:
-            unique_methods.append(dct['method'].strip())
-            unique_method_tokens.append(dct['tokens'])
-        
-        dct['foreign_key'] = str(unique_methods.index(dct['method'].strip()))
+    for i, _ in enumerate(pool.imap_unordered(process_file, source_paths), 1):
+        sys.stderr.write('\rpercentage of source code files completed: {0:%}'.format(i/len(source_paths)))
 
-        json_file.write(json.dumps(dct) + '\n')
-        json_file.flush()
-
-    assert len(unique_methods) == len(unique_method_tokens)
-
-    with open(f'data/{project}/unique_methods.jsonl', 'w') as fw:
-        for i in range(len(unique_methods)):
-            fw.write(json.dumps({'index': str(i), 'method': unique_methods[i], 'tokens': unique_method_tokens[i]}) + '\n')
-            fw.flush()
+    logging.info(f'total time in secs for {args.project_name}: ' + str(round(time.time() - start, 2)))
+    logging.info(f'total methods extracted from {args.project_name}: ' + str(counter.val.value))
 
 
 def parse_args():
     parser = argparse.ArgumentParser("extract methods of a given java project")
-    parser.add_argument('--project_name', type=str, default='JacksonXml', help='defects4j project name to process and extract methods')
+    parser.add_argument('--project_name', type=str, default='commons-cli', help='defects4j project name to process and extract methods')
+    parser.add_argument('--log_file', type=str, default='method_extractor.log', help='log file name for method extractor')
     return parser.parse_args()
 
 if __name__ == '__main__':
---
+++ scripts/extract_methods.sh
@@ -0,0 +1,30 @@
+# !/usr/bin/env bash
+
+export PYTHONIOENCODING=utf-8;
+
+function prompt() {
+    echo;
+    echo "Syntax: bash scripts/extract_methods.sh LOG_FILE_NAME";
+    echo "LOG_FILE_NAME is required";
+    exit;
+}
+
+while getopts ":h" option; do
+    case $option in
+        h) # display help
+          prompt;
+    esac
+done
+
+if [[ $# < 1 ]]; then
+  prompt;
+fi
+
+LOG_FILE_NAME=$1;
+
+# projects=("commons-cli" "jfreechart" "commons-codec" "commons-collections" "commons-compress" "commons-csv" "gson" "commons-lang" "commons-math" "commons-jxpath" "jackson-dataformat-xml" "jackson-core" "jackson-databind" "jsoup" "joda-time" "mockito")
+projects=("joda-time" "mockito")
+for project in "${projects[@]}"
+do
+    python3 method_extractor.py --project_name $project --log_file $LOG_FILE_NAME;
+done
