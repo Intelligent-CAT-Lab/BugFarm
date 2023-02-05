bbe5eea8d34aa7684e19fc2892a13c718bdff33c
alibrahimzada <alirezai@illinois.edu>
2023-02-05 10:21:19 -0600

    updated requirements

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@ function install_requirements() {
     ansi==0.3.6;
     git+https://github.com/jose/javalang.git@start_position_and_end_position;
     matplotlib==3.3.4;
-    numpy==1.21.6;
-    seaborn==0.12.2;
-    torch==1.12.1;
-    transformers==4.22.2;
+    nltk==3.6.7
+    numpy==1.21.6
+    seaborn==0.12.2
+    torch==1.12.1
+    transformers==4.22.2
+    wordninja==2.0.0
 }
 
 function download_projects() {
