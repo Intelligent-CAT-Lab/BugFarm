1950e3ba61585498abe70ff0694c708c90879816
alibrahimzada <alirezai@illinois.edu>
2023-03-11 17:21:32 -0600

    added finetuning scripts

--- utils.py
+++ utils.py
@@ -1,7 +1,48 @@
 import ast
 import numpy as np
 import seaborn as sns
+import time
+import os
+import torch
+from torch.utils.data import TensorDataset
+import logging
+import random
+from tqdm import tqdm
+import json
 from ansi.colour import rgb
+
+logger = logging.getLogger(__name__)
+
+class Example(object):
+    """A single training/test example."""
+
+    def __init__(self,
+                 idx,
+                 source,
+                 target,
+                 url=None,
+                 task='',
+                 sub_task=''
+                 ):
+        self.idx = idx
+        self.source = source
+        self.target = target
+        self.url = url
+        self.task = task
+        self.sub_task = sub_task
+
+
+class DefectInputFeatures(object):
+    """A single training/test features for a example."""
+
+    def __init__(self,
+                 example_id,
+                 source_ids,
+                 label
+                 ):
+        self.example_id = example_id
+        self.source_ids = source_ids
+        self.label = label
 
 
 def color_text(text, rgb_code):
@@ -346,3 +387,155 @@ def tokenize(fname):
         tokens.append((token, scope))
 
     return tokens
+
+
+def get_elapse_time(t0):
+    elapse_time = time.time() - t0
+    if elapse_time > 3600:
+        hour = int(elapse_time // 3600)
+        minute = int((elapse_time % 3600) // 60)
+        return "{}h{}m".format(hour, minute)
+    else:
+        minute = int((elapse_time % 3600) // 60)
+        return "{}m".format(minute)
+
+
+def get_filenames(data_root, task, sub_task, split=''):
+    if task == 'concode':
+        data_dir = '{}/{}'.format(data_root, task)
+        train_fn = '{}/train.json'.format(data_dir)
+        dev_fn = '{}/dev.json'.format(data_dir)
+        test_fn = '{}/test.json'.format(data_dir)
+    elif task == 'summarize':
+        data_dir = '{}/{}/{}'.format(data_root, task, sub_task)
+        train_fn = '{}/train.jsonl'.format(data_dir)
+        dev_fn = '{}/valid.jsonl'.format(data_dir)
+        test_fn = '{}/test.jsonl'.format(data_dir)
+    elif task == 'refine':
+        data_dir = '{}/{}/{}'.format(data_root, task, sub_task)
+        train_fn = '{}/train.buggy-fixed.buggy,{}/train.buggy-fixed.fixed'.format(data_dir, data_dir)
+        dev_fn = '{}/valid.buggy-fixed.buggy,{}/valid.buggy-fixed.fixed'.format(data_dir, data_dir)
+        test_fn = '{}/test.buggy-fixed.buggy,{}/test.buggy-fixed.fixed'.format(data_dir, data_dir)
+    elif task == 'translate':
+        data_dir = '{}/{}'.format(data_root, task)
+        if sub_task == 'cs-java':
+            train_fn = '{}/train.java-cs.txt.cs,{}/train.java-cs.txt.java'.format(data_dir, data_dir)
+            dev_fn = '{}/valid.java-cs.txt.cs,{}/valid.java-cs.txt.java'.format(data_dir, data_dir)
+            test_fn = '{}/test.java-cs.txt.cs,{}/test.java-cs.txt.java'.format(data_dir, data_dir)
+        else:
+            train_fn = '{}/train.java-cs.txt.java,{}/train.java-cs.txt.cs'.format(data_dir, data_dir)
+            dev_fn = '{}/valid.java-cs.txt.java,{}/valid.java-cs.txt.cs'.format(data_dir, data_dir)
+            test_fn = '{}/test.java-cs.txt.java,{}/test.java-cs.txt.cs'.format(data_dir, data_dir)
+    elif task == 'clone':
+        data_dir = '{}/{}'.format(data_root, task)
+        train_fn = '{}/train.txt'.format(data_dir)
+        dev_fn = '{}/valid.txt'.format(data_dir)
+        test_fn = '{}/test.txt'.format(data_dir)
+    elif task == 'defect':
+        data_dir = '{}/{}'.format(data_root, task)
+        train_fn = '{}/train.jsonl'.format(data_dir)
+        dev_fn = '{}/valid.jsonl'.format(data_dir)
+        test_fn = '{}/test.jsonl'.format(data_dir)
+    if split == 'train':
+        return train_fn
+    elif split == 'dev':
+        return dev_fn
+    elif split == 'test':
+        return test_fn
+    else:
+        return train_fn, dev_fn, test_fn
+
+
+def load_and_cache_defect_data(args, filename, pool, tokenizer, split_tag, is_sample=False):
+    cache_fn = os.path.join(args.cache_path, split_tag)
+    examples = read_examples(filename, args.data_num, args.task)
+    if is_sample:
+        examples = random.sample(examples, int(len(examples) * 0.1))
+
+    calc_stats(examples, tokenizer, is_tokenize=True)
+    if os.path.exists(cache_fn):
+        logger.info("Load cache data from %s", cache_fn)
+        data = torch.load(cache_fn)
+    else:
+        if is_sample:
+            logger.info("Sample 10 percent of data from %s", filename)
+        elif args.data_num == -1:
+            logger.info("Create cache data into %s", cache_fn)
+        tuple_examples = [(example, idx, tokenizer, args) for idx, example in enumerate(examples)]
+        features = pool.map(convert_defect_examples_to_features, tqdm(tuple_examples, total=len(tuple_examples)))
+        # features = [convert_clone_examples_to_features(x) for x in tuple_examples]
+        all_source_ids = torch.tensor([f.source_ids for f in features], dtype=torch.long)
+        all_labels = torch.tensor([f.label for f in features], dtype=torch.long)
+        data = TensorDataset(all_source_ids, all_labels)
+
+        if args.local_rank in [-1, 0] and args.data_num == -1:
+            torch.save(data, cache_fn)
+    return examples, data
+
+
+def read_examples(filename, data_num, task):
+    read_example_dict = {
+        # 'summarize': read_summarize_examples,
+        # 'refine': read_refine_examples,
+        # 'translate': read_translate_examples,
+        # 'concode': read_concode_examples,
+        # 'clone': read_clone_examples,
+        'defect': read_defect_examples,
+    }
+    return read_example_dict[task](filename, data_num)
+
+
+def calc_stats(examples, tokenizer=None, is_tokenize=False):
+    avg_src_len = []
+    avg_trg_len = []
+    avg_src_len_tokenize = []
+    avg_trg_len_tokenize = []
+    for ex in examples:
+        if is_tokenize:
+            avg_src_len.append(len(ex.source.split()))
+            avg_trg_len.append(len(str(ex.target).split()))
+            avg_src_len_tokenize.append(len(tokenizer.tokenize(ex.source)))
+            avg_trg_len_tokenize.append(len(tokenizer.tokenize(str(ex.target))))
+        else:
+            avg_src_len.append(len(ex.source.split()))
+            avg_trg_len.append(len(str(ex.target).split()))
+    if is_tokenize:
+        logger.info("Read %d examples, avg src len: %d, avg trg len: %d, max src len: %d, max trg len: %d",
+                    len(examples), np.mean(avg_src_len), np.mean(avg_trg_len), max(avg_src_len), max(avg_trg_len))
+        logger.info("[TOKENIZE] avg src len: %d, avg trg len: %d, max src len: %d, max trg len: %d",
+                    np.mean(avg_src_len_tokenize), np.mean(avg_trg_len_tokenize), max(avg_src_len_tokenize),
+                    max(avg_trg_len_tokenize))
+    else:
+        logger.info("Read %d examples, avg src len: %d, avg trg len: %d, max src len: %d, max trg len: %d",
+                    len(examples), np.mean(avg_src_len), np.mean(avg_trg_len), max(avg_src_len), max(avg_trg_len))
+
+
+def read_defect_examples(filename, data_num):
+    """Read examples from filename."""
+    examples = []
+    with open(filename, encoding="utf-8") as f:
+        for idx, line in enumerate(f):
+            line = line.strip()
+            js = json.loads(line)
+
+            code = ' '.join(js['func'].split())
+            examples.append(
+                Example(
+                    idx=js['idx'],
+                    source=code,
+                    target=js['target']
+                )
+            )
+            if idx + 1 == data_num:
+                break
+    return examples
+
+
+def convert_defect_examples_to_features(item):
+    example, example_index, tokenizer, args = item
+    if args.model_type in ['t5', 'codet5'] and args.add_task_prefix:
+        source_str = "{}: {}".format(args.task, example.source)
+    else:
+        source_str = example.source
+    code = tokenizer.encode(source_str, max_length=args.max_source_length, padding='max_length', truncation=True)
+    return DefectInputFeatures(example_index, code, example.target)
