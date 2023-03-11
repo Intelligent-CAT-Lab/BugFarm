cc343886cca63d8a82e9d24f8990a9b2bdb80350
alibrahimzada <alirezai@illinois.edu>
2023-03-11 17:18:58 -0600

    updated chatgpt with openai api

--- chatgpt.py
+++ chatgpt.py
@@ -3,28 +3,16 @@ import ast
 import time
 import argparse
 from tqdm import tqdm
-from revChatGPT.V1 import Chatbot
+import openai
 
 
 def main(args):
-
+    
     config = {}
-    with open('configs/chatgpt_configs.json', 'r') as f:
+    with open('configs/configs.json', 'r') as f:
         config = json.load(f)
 
-    auth_configs = {}
-
-    if args.auth_type == 'access_token':
-        auth_configs['access_token'] = config['access_token']
-    
-    elif args.auth_type == 'email':
-        auth_configs['email'] = config['email']
-        auth_configs['password'] = config['password']
-    
-    elif args.auth_type == 'session_token':
-        auth_configs['session_token'] = config['session_token']
-
-    chatbot = Chatbot(config=auth_configs)
+    openai.api_key = config['openai_api_key']
 
     lines = []
     with open(f'data/{args.project_name}/unique_methods_{args.model_type}-{args.model_size}_las_lat.jsonl', 'r') as f:
@@ -73,11 +61,9 @@ def main(args):
 
         prompt =  """Observe the following java method where each statement has a specific ID starting from 0. Can you produce 3 different buggy versions of this method by changing {} only? Do not change other statements in the given java code.\n\nYou have to write each buggy method again. Do not write anything else in your response. Make sure your generated buggy java code is compilable and does not have syntax errors and compile-time errors. Do not use a variable which does not exist in the scope of the given method. You should put <start1> <start2> <start3> and <end1> <end2> <end3> in the beginning and end of each buggy method so I could parse your response later.\n\n{}""".format(change_statements_str, '\n'.join(index_code))
 
-        response = ""
-        for data in chatbot.ask(prompt):
-            response = data["message"]
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
 
-        dct['chatgpt_response'] = response
+        dct['chatgpt_response'] = response.choices[0].message.content
         dct['duration'] = time.time() - start_time
 
         json_file.write(json.dumps(dct) + '\n')
@@ -89,7 +75,6 @@ def parse_args():
     parser.add_argument('--project_name', type=str, default='commons-cli', help='project name to process and extract methods')
     parser.add_argument('--model_type', type=str, default='codebert', help='LLM to use in this experiment')
     parser.add_argument('--model_size', type=str, default='base', help='model size to use in this experiment')
-    parser.add_argument('--auth_type', type=str, default='access_token', help='auth type to use in this experiment')
     return parser.parse_args()
 
 if __name__ == '__main__':
