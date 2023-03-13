781005ff3f7b1933a4ca3a5751e53ad1cb71e660
alibrahimzada <alirezai@illinois.edu>
2023-03-12 19:26:06 -0500

    removed refine dataset creation

--- create_defect_dataset.py
+++ create_defect_dataset.py
@@ -5,50 +5,28 @@ import ast
 
 def main(args):
 
-    if args.type == 'test':
-        json_file = open(f'data/defect/{args.size}/test.jsonl', 'wt')
+    projects = ["commons-cli", "commons-codec", "commons-collections", "commons-compress", "commons-csv", "commons-jxpath", "commons-lang", "commons-math", "gson", "jackson-core", "jackson-databind", "jackson-dataformat-xml", "jfreechart", "joda-time", "jsoup"]
 
-        counter = 0
-        with open(f'data/defect/{args.size}/data_refine_{args.size}_test.buggy-fixed.buggy', 'r') as f:
+    json_file = open(f'data/defect/ours/big_train.jsonl', 'wt')
+
+    counter = 0
+    for project in projects:
+
+        with open(f'data/{project}/unique_methods_codebert-base_selected_bugs.jsonl', 'r') as f:
             lines = f.readlines()
             for line in lines:
-                json_file.write(json.dumps({"func": f"{line.strip()}", "targe": 0, "idx": counter}) + '\n')
-                json_file.flush()
+                dct = ast.literal_eval(line)
+                json_file.write(json.dumps({"func": f"{dct['method']}", "target": 0, "idx": counter}) + '\n')
                 counter += 1
-        
-        with open(f'data/defect/{args.size}/data_refine_{args.size}_test.buggy-fixed.fixed', 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                json_file.write(json.dumps({"func": f"{line.strip()}", "label": 1, "idx": counter}) + '\n')
+
+                if 'selected_bugs' not in dct:
+                    continue
+
+                for bug_id in dct['selected_bugs']:
+                    json_file.write(json.dumps({"func": f"{dct[f'buggy_method{bug_id}']}", "target": 1, "idx": counter}) + '\n')
+                    counter += 1
+
                 json_file.flush()
-                counter += 1
-        
-        json_file.close()
-
-    elif args.type == 'train':
-
-        if args.subtype == 'ours':
-
-            # projects = ["commons-cli", "commons-codec", "commons-collections", "commons-compress", "commons-csv", "commons-jxpath", "commons-lang", "commons-math", "gson", "jackson-core", "jackson-databind", "jackson-dataformat-xml", "jfreechart", "joda-time", "jsoup"]
-            projects = ["commons-cli", "commons-codec", "commons-csv", "commons-jxpath", "gson", "jackson-core", "jackson-dataformat-xml", "jfreechart", "jsoup"]
-
-            json_file = open(f'data/defect/ours/train.jsonl', 'wt')
-
-            counter = 0
-            for project in projects:
-
-                with open(f'data/{project}/unique_methods_codebert-base_selected_bugs.jsonl', 'r') as f:
-                    lines = f.readlines()
-                    for line in lines:
-                        dct = ast.literal_eval(line)
-                        json_file.write(json.dumps({"func": f"{dct['method']}", "target": 1, "idx": counter}) + '\n')
-                        counter += 1
-
-                        for bug_id in dct['selected_bugs']:
-                            json_file.write(json.dumps({"func": f"{dct[f'buggy_method{bug_id}']}", "target": 0, "idx": counter}) + '\n')
-                            counter += 1
-
-                        json_file.flush()
 
 
 def parse_args():
