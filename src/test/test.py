d741be472ec769b27a4df035d9ee741bb1878637
alibrahimzada <alirezai@illinois.edu>
2023-04-21 16:43:26 -0500

    revert testing to multiprocessing

--- src/test/test.py
+++ src/test/test.py
@@ -1,78 +1,76 @@
 import ast
 import os
 import argparse
+import multiprocessing
+import sys
 
 
 def main(args):
-    global test_results
 
     lines = []
     with open(f'data/{args.project_name}/unique_methods_{args.model_name}_selected_bugs.jsonl') as f:
         lines = f.readlines()
 
-    test_results = open(f'data/{args.project_name}/test_results_{args.model_name}.txt', 'a')
-
     os.makedirs(f'test_results/{args.project_name}', exist_ok=True)
 
-    for l in lines:
+    pool = multiprocessing.Pool(args.num_workers)
+    for i, _ in enumerate(pool.imap_unordered(process_instance, lines), 1):
+        sys.stderr.write('\rpercentage of source code files completed: {0:%}'.format(i/len(lines)))
 
-        dct = ast.literal_eval(l)
 
-        index = dct['index']
+def process_instance(l):
 
-        if dct['selected_bugs'] == []:
+    dct = ast.literal_eval(l)
+
+    index = dct['index']
+
+    if dct['selected_bugs'] == []:
+        return
+
+    for bug_id in dct['selected_bugs']:
+        project = dct['project']
+
+        os.makedirs(f'temp_project_{index}', exist_ok=True)
+
+        if os.path.exists(f'test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log'):
             continue
 
-        for bug_id in dct['selected_bugs']:
-            project = dct['project']
+        os.system(f'cp -r projects/{project} temp_project_{index}/')
 
-            os.makedirs(f'temp_project', exist_ok=True)
+        buggy_method = dct[f'buggy_method{bug_id}']
 
-            if os.path.exists(f'test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log'):
-                continue
+        file_path = dct['file_path']
+        start_line = int(dct['start_line'])
+        end_line = int(dct['end_line'])
 
-            if not os.path.exists(f'temp_project/{project}'):
-                os.system(f'cp -r projects/{project} temp_project/{project}')
+        relative_path = file_path.split(f'{project}/')[1]
 
-            buggy_method = dct[f'buggy_method{bug_id}']
+        file_lines = []
+        with open(f'temp_project_{index}/{project}/{relative_path}', 'r') as f:
+            file_lines = f.readlines()
+        
+        file_lines[start_line-1:end_line] = buggy_method
 
-            file_path = dct['file_path']
-            start_line = int(dct['start_line'])
-            end_line = int(dct['end_line'])
+        with open(f'temp_project_{index}/{project}/{relative_path}', 'w') as f:
+            f.writelines(file_lines)
 
-            relative_path = file_path.split(f'{project}/')[1]
+        os.chdir(f'temp_project_{index}/{project}')
+        if project in ['commons-lang', 'joda-time']:
+            os.system(f'timeout 3600 JAVA_HOME=`/usr/libexec/java_home -v 1.8` mvn clean test -Drat.skip=true 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
+        else:
+            os.system(f'timeout 3600 mvn clean test -Drat.skip=true 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
 
-            file_lines = []
-            with open(f'temp_project/{project}/{relative_path}', 'r') as f:
-                file_lines = f.readlines()
-            
-            file_lines[start_line-1:end_line] = buggy_method
+        os.chdir('../../')
+        os.system(f'rm -rf temp_project_{index}/{project}')
 
-            with open(f'temp_project/{project}/{relative_path}', 'w') as f:
-                f.writelines(file_lines)
-
-            os.chdir(f'temp_project/{project}')
-            if project in ['commons-lang', 'joda-time']:
-                os.system(f'timeout 3600 JAVA_HOME=`/usr/libexec/java_home -v 1.8` mvn test -Drat.skip=true | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
-            else:
-                os.system(f'timeout 3600 mvn test -Drat.skip=true | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
-
-            os.chdir('../../')
-
-            file_lines = []
-            with open(file_path, 'r') as f:
-                file_lines = f.readlines()
-            
-            with open(f'temp_project/{project}/{relative_path}', 'w') as f:
-                f.writelines(file_lines)
-
-    os.system(f'rm -rf temp_project')
+    os.rmdir(f'temp_project_{index}')
 
 
 def parse_args():
     parser = argparse.ArgumentParser("test buggy methods on projects")
     parser.add_argument('--project_name', type=str, default='commons-cli', help='project name to test buggy methods on')
     parser.add_argument('--model_name', type=str, default='codebert-base', help='model name to test buggy methods on')
+    parser.add_argument('--num_workers', type=int, default=8, help='number of cpu cores to use for threading')
     return parser.parse_args()
 
 if __name__ == '__main__':
