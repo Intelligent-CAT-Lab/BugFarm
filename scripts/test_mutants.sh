42170f96b99a2bbdc94266821bee668d219f08df
alibrahimzada <alirezai@illinois.edu>
2023-05-10 23:49:21 -0500

    removed multiprocessing from testing

--- scripts/test_mutants.sh
+++ scripts/test_mutants.sh
@@ -6,11 +6,10 @@ export PYTHONIOENCODING=utf-8;
 
 function prompt() {
     echo;
-    echo "Syntax: bash scripts/test_mutants.sh PROJECT_NAME MODEL_NAME MODEL_SIZE NUM_WORKERS";
+    echo "Syntax: bash scripts/test_mutants.sh PROJECT_NAME MODEL_NAME MODEL_SIZE";
     echo "PROJECT_NAME is required [one of commons-cli, commons-codec, commons-collections, commons-compress, commons-csv, commons-jxpath, commons-lang, commons-math, gson, jackson-core, jackson-databind, jackson-dataformat-xml, jfreechart, joda-time, jsoup]";
     echo "MODEL_NAME is required [one of codebert, codet5, NatGen]";
     echo "MODEL_SIZE is required [base]";
-    echo "NUM_WORKERS is required";    
     exit;
 }
 
@@ -21,13 +20,12 @@ while getopts ":h" option; do
     esac
 done
 
-if [[ $# < 4 ]]; then
+if [[ $# < 3 ]]; then
   prompt;
 fi
 
 PROJECT_NAME=$1;
 MODEL_NAME=$2;
 MODEL_SIZE=$3;
-NUM_WORKERS=$4;
 
-python3 src/test/test.py --project_name $PROJECT_NAME --model_name $MODEL_NAME-$MODEL_SIZE --num_workers $NUM_WORKERS;
+python3 src/test/test.py --project_name $PROJECT_NAME --model_name $MODEL_NAME --model_size $MODEL_SIZE;
--- src/test/test.py
+++ src/test/test.py
@@ -1,76 +1,89 @@
 import ast
 import os
 import argparse
-import multiprocessing
-import sys
+import logging
+import time
+from tqdm import tqdm
 
 
 def main(args):
 
     lines = []
-    with open(f'data/{args.project_name}/unique_methods_{args.model_name}_selected_bugs.jsonl') as f:
+    with open(f'data/{args.project_name}/unique_methods_{args.model_name}-{args.model_size}_selected_bugs.jsonl') as f:
         lines = f.readlines()
 
-    os.makedirs(f'test_results/{args.project_name}', exist_ok=True)
+    os.makedirs(f'logs', exist_ok=True)
+    logging.basicConfig(filename=f"logs/testing.log", level=logging.INFO, format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(f'testing project={args.project_name} model={args.model_name} size={args.model_size}')
 
-    pool = multiprocessing.Pool(args.num_workers)
-    for i, _ in enumerate(pool.imap_unordered(process_instance, lines), 1):
-        sys.stderr.write('\rpercentage of source code files completed: {0:%}'.format(i/len(lines)))
+    start_time = time.time()
 
+    project = args.project_name
 
-def process_instance(l):
+    os.makedirs(f'test_results/{project}', exist_ok=True)
+    os.makedirs(f'temp_project_{project}', exist_ok=True)
+    os.system(f'cp -r projects/{project} temp_project_{project}/')
 
-    dct = ast.literal_eval(l)
+    for l in tqdm(lines):
 
-    index = dct['index']
+        dct = ast.literal_eval(l)
 
-    if dct['selected_bugs'] == []:
-        return
+        index = dct['index']
 
-    for bug_id in dct['selected_bugs']:
-        project = dct['project']
-
-        os.makedirs(f'temp_project_{index}', exist_ok=True)
-
-        if os.path.exists(f'test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log'):
+        if dct['selected_bugs'] == []:
             continue
-
-        os.system(f'cp -r projects/{project} temp_project_{index}/')
-
-        buggy_method = dct[f'buggy_method{bug_id}']
-
-        file_path = dct['file_path']
-        start_line = int(dct['start_line'])
-        end_line = int(dct['end_line'])
-
-        relative_path = '/'.join(file_path.split('/')[1:])
-
-        file_lines = []
-        with open(f'temp_project_{index}/{relative_path}', 'r', encoding="ISO-8859-1", errors='ignore') as f:
-            file_lines = f.readlines()
         
-        file_lines[start_line-1:end_line] = buggy_method
+        # read original file
+        file_path = dct['file_path']
+        relative_path = '/'.join(file_path.split('/')[1:])
+        file_lines = []
+        with open(f'temp_project_{project}/{relative_path}', 'r', encoding="ISO-8859-1", errors='ignore') as f:
+            file_lines = f.readlines()
 
-        with open(f'temp_project_{index}/{relative_path}', 'w') as f:
-            f.writelines(file_lines)
+        for bug_id in dct['selected_bugs']:
+            project = dct['project']
 
-        os.chdir(f'temp_project_{index}/{project}')
-        if project in ['commons-lang', 'joda-time']:
-            os.system(f'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre" timeout 3600 mvn clean test -Drat.skip=true 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
-        else:
-            os.system(f'timeout 3600 mvn clean test -Drat.skip=true 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}.build.log')
+            if os.path.exists(f'test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}-{args.model_size}.build.log'):
+                continue
 
-        os.chdir('../../')
-        os.system(f'rm -rf temp_project_{index}/{project}')
+            buggy_method = dct[f'buggy_method{bug_id}']
 
-    os.rmdir(f'temp_project_{index}')
+
+            file_path = dct['file_path']
+            
+            file_path = dct['file_path']
+            start_line = int(dct['start_line'])
+            end_line = int(dct['end_line'])
+
+            buggy_file_lines = file_lines.copy()
+            buggy_file_lines[start_line-1:end_line] = buggy_method
+
+            with open(f'temp_project_{project}/{relative_path}', 'w') as f:
+                f.writelines(buggy_file_lines)
+
+            os.chdir(f'temp_project_{project}/{project}')
+            if project in ['commons-lang', 'joda-time']:
+                os.system(f'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre" timeout 300 mvn test -Drat.skip=true -Dmaven.compiler.useIncrementalCompilation=false 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}-{args.model_size}.build.log')
+            else:
+                os.system(f'timeout 300 mvn test -Drat.skip=true -Dmaven.compiler.useIncrementalCompilation=false 2> /dev/null | grep ERROR > ../../test_results/{project}/{project}.{index}.{bug_id}.{args.model_name}-{args.model_size}.build.log')
+
+            os.chdir('../../')
+
+            with open(f'temp_project_{project}/{relative_path}', 'w') as f:
+                f.writelines(file_lines)
+
+    end_time = time.time()
+    time_minutes = round((end_time-start_time)/60,2)
+    logging.info(f'testing project={args.project_name} model={args.model_name} size={args.model_size} time={round(end_time-start_time,2)} seconds ({time_minutes} minutes)')
+
+    os.system(f'rm -rf temp_project_{project}')
 
 
 def parse_args():
     parser = argparse.ArgumentParser("test buggy methods on projects")
     parser.add_argument('--project_name', type=str, default='commons-cli', help='project name to test buggy methods on')
-    parser.add_argument('--model_name', type=str, default='codebert-base', help='model name to test buggy methods on')
-    parser.add_argument('--num_workers', type=int, default=8, help='number of cpu cores to use for threading')
+    parser.add_argument('--model_name', type=str, default='codebert', help='model name to test buggy methods on')
+    parser.add_argument('--model_size', type=str, default='base', help='model size to test buggy methods on')
     return parser.parse_args()
 
 if __name__ == '__main__':
